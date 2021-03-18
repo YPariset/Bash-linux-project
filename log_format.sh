b4470513fd23ee24447d0119014a3030f6d318b2
YPariset <yoann.pariset@edu.itescia.fr>
2021-03-18 11:01:20 +0000

    story 3 done done

--- log_format.sh
+++ log_format.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-cat $1 | while read line
+#Permet de creer une variable date et de la formater YYYY-MM-DD-HH-MM-SS  
+varDate=$(date '+%Y-%m-%d-%H-%M-%S')
+
+mkdir -p jsonFolder
+touch jsonFolder/log-$varDate.json
+
+cat $1 | while read line 
 
 do
 
 type=$(echo $line | awk '{print ($3)}')
 msg=$(echo $line | awk '{print ($4$5$6$7$8$9$10)}')
 date=$(echo $line | awk '{print ($1" "$2)}'| cut -c2-20| date +%F" "%T)
-echo {\"type\":\"$type\"','"\"msg\":\"$msg\",\"time\":\"$date\""}
+echo {\"type\":\"$type\"','"\"msg\":\"$msg\",\"time\":\"$date\""} >> jsonFolder/log-$varDate.json
 
 done
