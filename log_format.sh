65365b4bbe680172578d6d8f9bd885d6a819014c
YPariset <yoann.pariset@edu.itescia.fr>
2021-03-18 09:04:34 +0000

    awk used to parse json

--- log_format.sh
+++ log_format.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-jq -R '[ split("\n")[] | select(length > 0) | split(" ") | {type: .[2], msg: .[3], time: .[0] }]' $1
+cat $1 | while read line
+
+do
+
+type=$(echo $line | awk '{print ($3)}')
+msg=$(echo $line | awk '{print ($4$5$6$7$8$9$10)}')
+date=$(echo $line | awk '{print ($1" "$2)}')
+echo {\"type\":\"$type\"','"\"msg\":\"$msg\",\"time\":\"$date\""}
+
+done
