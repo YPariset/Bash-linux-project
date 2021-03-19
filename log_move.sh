b3189e7915dec4c647cd2197283efb5adf6c26a0
YPariset <yoann.pariset@edu.itescia.fr>
2021-03-19 08:05:44 +0000

    extraction tar done

--- log_move.sh
+++ log_move.sh
@@ -4,4 +4,6 @@ varFolderDate=$(date '+%Y_%m_%d')
 
 ./log_dump.sh /workspace/log_server/server.log /var/www/html/log/$varFolderDate/ && ./log_filter.sh /var/www/html/log/$varFolderDate/*.log >> tmp.log && ./log_format.sh tmp.log /var/www/html/log/$varFolderDate/    
 echo /var/www/html/log/$varFolderDate/*.log  >> /var/www/html/log/dump_list.txt 
-echo /var/www/html/log/$varFolderDate/*.json  >> /var/www/html/log/json_list.txt 
+echo /var/www/html/log/$varFolderDate/*.json  >> /var/www/html/log/json_list.txt
+
+tar -cvzf log.tar.gz /var/www/html/log && mv log.tar.gz /var/www/html/log.tar.gzâ
